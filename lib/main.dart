94e7e7e617925a0c9afa79ddafcb8976802b57b5
Alexis Dalle <itsalexousd@gmail.com>
2024-05-15 04:44:41 +0200

    ex3

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,54 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    if (counter > 0) {
+      setState(() {
+        counter--;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Exercice 2'),
+          title: const Text('Exercice 3'),
         ),
-        body: const Column(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('Compteur: $counter'),
+            const SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Flutter'),
-                SizedBox(width: 10),
-                Text('est génial !'),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Apprendre'),
-                SizedBox(width: 10),
-                Text('Flutter'),
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  child: const Text('+'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _decrementCounter,
+                  child: const Text('-'),
+                ),
               ],
             ),
           ],
